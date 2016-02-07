1fc3ebb19ae9ccf526521a85d4f38874be277cac
Isaac Siegel <siegelhorn@gmail.com>
2016-02-06 21:35:44 -0800

    Added skeleton function for imgur testing

--- memeoff/www/js/data-service.js
+++ memeoff/www/js/data-service.js
@@ -1,16 +1,29 @@
-app.factory('Data', function() {
+app.factory('Data', ['$http', function($http) {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
   var memes = [{
     url: "http://i.imgur.com/gDST0yjm.jpg",
-    caption: 'Awesome Cssdsdon asdasd',
+    caption: "Noah's Ark V2: God Also Dislikes Fossil Fuels",
     score: 5
   }, {
     url: "http://i.imgur.com/gDST0yjm.jpg",
     caption: 'Awesome Caption2',
-    score: 7
+    score: 4
   }];
+
+  var uploadToImgur = function(imageBase64) {
+    $http({
+      method: 'POST',
+      url: '/someUrl'
+    }).then(function successCallback(response) {
+        // NEED TO GET IMGUR URL OUT OF THE RESPONSE
+      }, function errorCallback(response) {
+
+      });
+  }
+
+  uploadToImgur("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==")
 
   return {
     getLeaderboard: function() {
@@ -28,5 +41,6 @@ app.factory('Data', function() {
       return memes;
     }
 
+
   };
-});
+}]);
